34dfd1677f3c708535196acaef669a11d869a257
Jaysonliu <leobest9688@gmail.com>
2021-10-19 23:51:32 +0800

    set database

---
+++ src/Command/Database.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+class Database
+{
+   static public function getDatabaseUrl(){
+    return 'mysql://ba62ffb01ea84f:93d51e2d@us-cdbr-east-03.cleardb.com:3306/heroku_0e33b230f8548fc?serverVersion=5.7';
+    
+
+   }
+}
