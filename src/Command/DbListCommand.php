dcc2a247a22a4f9642d048aee1d9b9043054336a
Jaysonliu <leobest9688@gmail.com>
2021-10-20 23:53:41 +0800

    add dbInit

--- src/Command/DbListCommand.php
+++ src/Command/DbListCommand.php
@@ -1,49 +1,86 @@
 <?php
 
-// src/Command/CreateUserCommand.php
-namespace App\Command;
-
+namespace App;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
-class CreateUserCommand extends Command
+
+class DbListCommand extends Command
 {
-    // the name of the command (the part after "bin/console")
-    protected static $defaultName = 'app:create-user';
-    protected static $defaultDescription = 'Creates a new user.';
+    protected static $defaultName = 'db:list';
+    protected static $defaultDescription = 'Show the records.';
+
+    private $conn;
+
+    public function __construct(){
+        
+        $connectionParams = [
+            'url' => Database::getDatabaseUrl()
+        ];
+        $this->conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams);
+        parent::__construct();
+
+    }
 
     protected function configure(): void
     {
         $this
-            // If you don't like using the $defaultDescription static property,
-            // you can also define the short description using this method:
-            // ->setDescription('...')
-
-            // the command help shown when running the command with the "--help" option
-            ->setHelp('This command allows you to create a user...')
+            ->setHelp('Show datas form a table.')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // ... put here the code to create the user
+        
 
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
+        $io = new SymfonyStyle($input, $output);
 
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
+        $sm = $this->conn->getSchemaManager();      
+        //======1. Get the table;=========
+        $tables = $sm->listTables();
+        $tbs=[];
+        foreach ($tables as $key => $value) {
+            $tbs[]=$value->getName();
+        }
+        if(!$tbs){
+
+            $io->error("NO Tables.");
+
+        }else{
+
+            $queryBuilder = $this->conn->createQueryBuilder();
+
+            $helper = $this->getHelper('question');
+            $table = new ChoiceQuestion('Please select the table',
+            $tbs,0);
+            $table->setErrorMessage('Table %s is not exited.');
+            $table_name = $helper->ask($input, $output, $table);
+            $res=$queryBuilder->select('*')->from($table_name)->fetchAllAssociative();
+
+            //Get the columns
+            $columns = $sm->listTableColumns($table_name);
+            $header_fields=[];
+            foreach ($columns as $column) {     
+                $header_fields[]=$columnname=$column->getName();               
+                
+            }
+
+            $arr_cols=[];
+            foreach ($res as $key => $value) {               
+               $arr_cols[]=$value;
+            }
+        
+            $io->table($header_fields,$arr_cols);
+
+        }
+
+
+      
+      
+        
         return Command::SUCCESS;
-
-        // or return this if some error happened during the execution
-        // (it's equivalent to returning int(1))
-        // return Command::FAILURE;
-
-        // or return this to indicate incorrect command usage; e.g. invalid options
-        // or missing arguments (it's equivalent to returning int(2))
-        // return Command::INVALID
     }
-
-    
 }
